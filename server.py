de8edbb6e9da1b54eace116d027aa6ee040cdcb5
Janet Anne Panen <janetannepanen@Janets-MacBook-Pro.local>
2018-08-07 16:26:34 -0700

    Got Google Oauth working to get read access to Google Photos

---
+++ server.py
@@ -0,0 +1,90 @@
+from flask import Flask, redirect, request, render_template, session, url_for
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+import os
+import server
+import unittest
+import requests
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+CLIENT_SECRETS_FILE = 'google_oauth_client_secret.json'
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
+API_SERVICE_NAME = 'library'
+API_VERSION = 'v1'
+
+app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
+app.jinja_env.auto_reload = True
+
+app.secret_key = "JUSTABUNCHAGIBBERISH"
+
+@app.route('/')
+def shows_homepage():
+
+    return render_template('home.html')
+
+@app.route('/authorize')
+def authorize_user():
+    """For authorizing user to use Google Photos Library API."""
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE,
+        scopes=SCOPES)
+
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
+
+    authorization_url, state = flow.authorization_url(
+    # Enable offline access so that you can refresh an access token without
+    # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+    # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true')
+
+    # Store the state so the callback can verify the auth server response.
+    session['state'] = state
+
+    return redirect(authorization_url)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+    # Specify the state when creating the flow in the callback so that it can
+    # verified in the authorization server response.
+    state = session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+          CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    # Store credentials in the session.
+    # ACTION ITEM: In a production app, you likely want to save these
+    #              credentials in a persistent database instead.
+    credentials = flow.credentials
+    session['credentials'] = credentials_to_dict(credentials)
+
+    return redirect('/')
+
+def credentials_to_dict(credentials):
+
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
+
+if __name__ == '__main__':
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    #     When running in production *do not* leave this option enabled.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+    # Specify a hostname and port that are set as a valid redirect URI
+    # for your API project in the Google API Console.
+    app.run('0.0.0.0', 5000, debug=True)
